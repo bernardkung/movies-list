3ee9ea2a0e96acf6c9f5f7f78eb77c436e06d9fe
bernardkung <bpkung@gmail.com>
2025-02-27 21:42:13 -0500

    rewiring cookies

---
+++ README.md
@@ -0,0 +1,34 @@
+# Movie Lists
+
+## Link to Demo
+[bernardkung.github.io/movies-list](https://bernardkung.github.io/movies-list)
+
+
+## Description
+Remaking AFI's top 100 movies sites. Just a movie checklist.
+
+## Updates
+Now includes a cookie for saving progress between sessions.
+
+## Notes
+Build out cookie handling
+- cookie consent
+- cookie destroy
+
+Swap from movie rank as ID to imdb ID.
+
+Add alternative lists.
+
+Fast uncheck-all function. Relate it to cookie destroy?
+
+Filter for watched/unwatched movies.
+
+Search bar?
+
+
+
+
+## Sources
+AFI'S 100 YEARS...100 MOVIES — 10TH ANNIVERSARY EDITION
+
+https://www.afi.com/afis-100-years-100-movies-10th-anniversary-edition/
---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState, useEffect, useMemo } from 'react'
+import { CookiesProvider, useCookies } from 'react-cookie'
+import './App.css'
+import movies from './afi_list.json'
+
+
+function App() {
+  const [ cookies, setCookie ] = useCookies(['watched'])
+  const [ watchList, setWatchList ] = useState([])
+
+  const testMemo = useMemo(()=>{
+    return cookies.watched ? cookies.watched.length : 0
+  }, [cookies])
+
+  function updateList(item, list) {
+    if (list.includes(item)) {
+      return list.filter(i => i != item)
+    }
+    return [...list, item]
+  }
+
+  function updateCookie (watched) {
+    setCookie('watched', watched, { path: '/' })
+  }
+
+  const onClick = (e)=>{
+    console.log["test"]
+    const uList = updateList(e.target.value, watchList)
+    setWatchList(uList)
+  }
+
+  useEffect(()=>{
+    updateCookie(watchList)
+  }, [watchList])
+
+  useEffect(()=>{
+    if (cookies.watched) {
+      setWatchList(cookies.watched)
+    }
+  }, [])
+
+
+  const movieList = movies.map((movie, m)=>{
+    const inputKey = `${m}`
+    return (
+      <CookiesProvider key={`c${m}`}>
+        <li key={`m${m}`}>
+          <input 
+            type="checkbox" 
+            id={inputKey}
+            name={inputKey}
+            key={inputKey}
+            onChange={onClick}
+            value={movie['rank']}
+            checked={watchList.includes(movie['rank'].toString())}
+            // checked={true}
+          />
+          <label 
+            htmlFor={inputKey}
+            key={`l${m}`}
+          >
+            <span key={`r${m}`}>{movie.rank}.</span>
+            <span key={`t${m}`}>{movie.title}</span> 
+          </label>
+        </li>
+      </CookiesProvider>
+    )
+  })
+
+  return (
+    <>
+      <div className="header">
+        <span className={'counter'}>{ watchList.length }</span> 
+        <span className={'title'}>AFI Top 100 Movies</span>
+      </div>
+      <ul>
+        {movieList}
+      </ul>
+      <div className="header">
+        <span className={'counter'}>{ watchList.length }</span> 
+        <span className={'title'}>AFI Top 100 Movies</span>
+      </div>
+    </>
+  )
+}
+
+export default App
